db9926072bd5b8bd61996b78619af220d671fd45
Bryan Bond <bbond@student.neumont.edu>
2020-02-06 23:39:57 -0700

    added audio Files, CSS, HTML and Java

---
+++ main.js
@@ -0,0 +1,34 @@
+
+    let anote = document.getElementById('a_note_box')
+    anote.addEventListener('click', function() {
+        document.getElementById('a_note_audio').play(); 
+    })
+
+    let bnote = document.getElementById('b_note_box')
+    bnote.addEventListener('click', function() {
+        document.getElementById('b_note_audio').play()
+    })
+
+    let cnote = document.getElementById('c_note_box')
+    cnote.addEventListener('click', function() {
+        document.getElementById('c_note_audio').play();
+    })
+    let dnote = document.getElementById('d_note_box')
+    dnote.addEventListener('click', function() {
+        document.getElementById('d_note_audio').play();
+    })
+
+    let enote = document.getElementById('e_note_box')
+    enote.addEventListener('click', function() {
+        document.getElementById('e_note_audio').play();
+    })
+
+    let fnote = document.getElementById('f_note_box')
+    fnote.addEventListener('click', function() {
+        document.getElementById('f_note_audio').play();
+    })
+
+    let gnote = document.getElementById('g_note_box')
+    gnote.addEventListener('click', function() {
+        document.getElementById('g_note_audio').play();
+    })
